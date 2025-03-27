3d893b4362f05d826152dee5ec483c3fe8848288
= <=>
2025-03-27 15:53:07 +0300

    json stuff

--- static/scripts.js
+++ static/scripts.js
@@ -1,12 +1,14 @@
+const { json } = require("express");
+
 // In scripts.js
 document.getElementById("uploadForm").addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   
   try {
-    const response = (await fetch('/', {method: 'POST', body: formData})).json();
-    console.log(response.answer)
-    console.log(response.graph)
+    const response = (await fetch('/', {method: 'POST', body: formData}));
+    const jsonData = response.json();
+    console.log(jsonData);
     const html = await response.text();
     document.getElementById("results").innerHTML = html;
   } catch (error) {

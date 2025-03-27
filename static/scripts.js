24bf376914577534045477047191b68a2d598493
= <=>
2025-03-27 15:51:34 +0300

    YOOOO

--- static/scripts.js
+++ static/scripts.js
@@ -4,8 +4,9 @@ document.getElementById("uploadForm").addEventListener("submit", async (e) => {
   const formData = new FormData(e.target);
   
   try {
-    const response = await fetch('/', {method: 'POST', body: formData});
-    console.log(response)
+    const response = (await fetch('/', {method: 'POST', body: formData})).json();
+    console.log(response.answer)
+    console.log(response.graph)
     const html = await response.text();
     document.getElementById("results").innerHTML = html;
   } catch (error) {

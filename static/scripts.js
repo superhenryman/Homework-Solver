e9cbe085220a12d357edeec7fce0c217266cd915
= <=>
2025-03-27 12:56:18 +0300

    fronted

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,31 @@
 let deleteImage = () => {
     fetch("/free").then(response => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Error: response is bad');
         }
-        return response.json();  // Parse JSON data
+        return response.json();
       })
-      .then(data => console.log(data))  // Use the data from the response
-      .catch(error => console.error('There was a problem with the fetch operation:', error));
+      .then(data => console.log(data)) 
+      .catch(error => console.error('Error:', error));
 }
+
+document.getElementById("uploadForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  let fileInput = document.getElementById("fileInput");
+  if (!fileInput.files.length) {
+      alert("Please select a file first!");
+      return;
+  }
+
+  let formData = new FormData();
+  formData.append("file", fileInput.files[0]);
+
+  fetch("/", {
+      method: "POST",
+      body: formData
+  })
+  .then(response => response.json())
+  .then(data => console.log("Success:", data))
+  .catch(error => console.error("Error:", error));
+});
